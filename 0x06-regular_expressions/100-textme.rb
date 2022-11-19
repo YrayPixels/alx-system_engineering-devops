16464c0620b77aeedf8b310daeaaeed18b0530c2
Moses Erhinyodavwe <54286496+YrayPixels@users.noreply.github.com>
2022-11-18 22:49:43 -0400

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:|to:|flags:)[\w|\+|\-|\:]*(?=\])/).join(",")
+# A regular expression that matches a particualr pattern of texts
+# puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',') - Also possible solution
+
+from = ARGV[0].scan(/from:(.*?)\]/)
+to = ARGV[0].scan(/to:(.*?)\]/)
+flags = ARGV[0].scan(/flags:(.*?)\]/)
+puts [from, to, flags].join(',')
